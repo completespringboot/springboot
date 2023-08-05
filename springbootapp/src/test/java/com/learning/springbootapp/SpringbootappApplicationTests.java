df70baeeb193ec78bab5a596c66deb64c5784c31
dekirukuroda <dekirukuroda@gmail.com>
2023-08-05 19:47:50 +0900

    basic springboot application.

---
+++ springbootapp/src/test/java/com/learning/springbootapp/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
